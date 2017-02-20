cd632bb0111491c65287666143b50c9d01930947
leorebus <rebus@hotmail.it>
2017-02-20 12:32:06 +0100

    new page structure

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,20 @@
 var React = require('react');
 
 import Header from './components/Header';
+import Description from './components/Description';
 import ArticlesList from './components/ArticlesList';
 
 var Home = React.createClass({
-
-  componentWillMount: function () {
-  },
-
-  componentDidUpdate: function (prevProps, prevState) {
-  },
 
   render: function () {
     return (
       <div>
         <Header />
-        <ArticlesList />
+        <div className="doormat"></div>
+        <div className="l-row">
+          <ArticlesList />
+          <Description />
+        </div>
       </div>
     )
   }
