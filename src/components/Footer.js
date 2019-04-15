9cb0a304ec106b454ef3060907babd372a439999
Leo <rebus@hotmail.it>
2019-04-15 19:17:48 +0100

    new iban

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ var App = React.createClass({
         <p>
           <span className="footer__el">via Fiorentina, 1219 I 50036 Pratolino (Firenze)</span>
           &nbsp;&bull; <span className="footer__el">CF 90028910488</span>
-          &nbsp;&bull; <span className="footer__el">IBAN: IT16F0616038130100000000316</span>
+        &nbsp;&bull; <span className="footer__el">IBAN: IT63Z0306938132100000000316 (BIC: BCITITMM)</span>
         </p>
         <p>Copyright &copy; 2016 Associazione No early marriage - No al matrimonio precoce Onlus All Rights Reserved</p>
         <p>Creato da <a href="https://onga.ro">Leonardo Ongaro</a>
